63f5fd57e745a71a096c884f3b9e30851237dad9
Zoltán Fraknói <fraknoiz@gmail.com>
2019-03-18 13:04:29 +0100

    motor direction fix

--- line_follow_robot/main.cpp
+++ line_follow_robot/main.cpp
@@ -6,22 +6,22 @@
 
 #include "AccelStepper.h"
 
-LedControl led1(10);
-LedControl led2(11);
+// LedControl led1(10);
+// LedControl led2(11);
 LedControl builtInLed(13);
 IRSensorControl lineSensor(7);
 
-AccelStepper leftMotor(1, 5, 4); // pin 5 = step, pin 4 = direction
-AccelStepper rightMotor(1, 11, 10); // pin 11 = step, pin 10 = direction
+AccelStepper leftMotor(1, 5, 4); // 1 = driver,  pin 5 = step, pin 4 = direction
+AccelStepper rightMotor(1, 11, 10); //1 = driver,  pin 11 = step, pin 10 = direction
 
 
 void setup()
 {
     leftMotor.setMaxSpeed(400);
-    leftMotor.setSpeed(400);
+    leftMotor.setSpeed(-400);
 
     rightMotor.setMaxSpeed(400);
-    rightMotor.setSpeed(200);
+    rightMotor.setSpeed(400);
 
 }
 
@@ -29,17 +29,18 @@ void loop()
 {
     if (lineSensor.detectLine()) {
         builtInLed.TurnOn();
-        leftMotor.setSpeed(400);
+        leftMotor.setSpeed(-400);
         rightMotor.setSpeed(200);
     } else
     {
-        leftMotor.setSpeed(200);
-        rightMotor.setSpeed(400);
         builtInLed.TurnOff();
+        leftMotor.setSpeed(-200);
+        rightMotor.setSpeed(400);
     }
 
     rightMotor.runSpeed();
     leftMotor.runSpeed();
 }
+
 
 
